bd81b41289cb590a87de98cd6af160c0688463d2
root <martijavi56@gmail.com>
2022-02-11 13:24:24 +0100

    Nuevo componente para listado de instancias

--- src/components/CartaInstancia.vue
+++ src/components/CartaInstancia.vue
@@ -2,7 +2,8 @@
     <div class="card" @click="seleccionar()">
         <div class="card-body">
             <img :src="instancia.Plantillas.logo" class="card-img-top" alt="..." style="width: 10%; margin-bottom: 10px; filter: grayscale(100%);">
-            <h5 class="card-title">{{instancia.nombre}}</h5>
+            <h5 class="card-title"><span class="texto-azul">Nombre:</span> {{instancia.nombre}}</h5>
+            <h5 class="card-title"><span class="texto-azul">Descripcion:</span> {{instancia.descripcion}}</h5>
         </div>
     </div>
 </template>
@@ -24,5 +25,8 @@ export default {
 <style scoped>
 .card:hover {
     border-color: #6C63FF;
+}
+.card-title {
+    margin-top: 10px;
 }
 </style>
